8463e906a696f216e47b5169293c41099c96bd44
anuprshetty <anuprshetty@gmail.com>
2023-03-12 00:20:40 +0530

    deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-
+ 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
@@ -13,4 +12,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-};
+}
